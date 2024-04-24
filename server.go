931897a1c1b929149acefe1543d0e3a754e70b3d
Smidra <rad.smid@gmail.com>
2024-04-24 22:08:20 +0200

    Extend test endpoint.

--- server.go
+++ server.go
@@ -11,5 +11,8 @@ func main() {
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
 	})
+	e.GET("/tony", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Ahojky Tony!")
+	})
 	e.Logger.Fatal(e.Start(":1323"))
 }
